ff94051b71cc5522dcf0a840bb76fb853f7c37f3
Yuji OKAMOTO <okamyuji@gmail.com>
2025-12-06 15:16:36 +0900

    fix: prettier format

--- frontend/src/components/fields/index.ts
+++ frontend/src/components/fields/index.ts
@@ -2,9 +2,8 @@ export { FieldEditor, type FieldFormData } from "./FieldEditor";
 export {
   FieldItemOverlay,
   FieldPalette,
-  PaletteItemOverlay
+  PaletteItemOverlay,
 } from "./FieldPalette";
 export { FieldRenderer } from "./FieldRenderer";
 export { fieldTypeIcons } from "./fieldTypeIcons";
 export { FieldValue } from "./FieldValue";
-
